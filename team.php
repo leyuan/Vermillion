5e7472822261bfe6364e6ef321c8b5c40dbdd356
Leyuan <leyuan.ly@outlook.com>
2015-03-31 23:13:50 -0600

    hatches & finish the skeleton for team page

--- team.php
+++ team.php
@@ -68,7 +68,61 @@
 						</p>
 					</div>
 				</div>
+
+				<div class="body container">
+					<h2 class="p-color font-light">Why us?</h2>
+					<div class="text-center">
+						<div class="col-md-4">
+							<h3>Experienced</h3>
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+						</div>
+						<div class="col-md-4">
+							<h3>The Best</h3>
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+						</div>
+						<div class="col-md-4">
+							<h3>Trustworthy</h3>
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+						</div>
+					</div>
+				</div>
 			</div>
+		</div>
+
+		<div class="sub-row team-history white-font">
+			<div class="container text-center">
+				<div class="head">
+					<h2>Previous Accomplishments</h2>
+					<p>A caption introduces the previous projects</p>			
+				</div>	
+				<div class="body">
+					<div class="accomplishment-row">
+						<div class="col-md-6">
+							<img src="assets/images/accomplishment1.png" alt="Hospital" class="center accomplishment-img" />
+						</div>
+						<div class="col-md-6">
+							<h3>Medical Hospital of China</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+							</p>
+						</div>
+					</div>
+					<div class="accomplishment-row">
+						<div class="col-md-6">
+							<img src="assets/images/accomplishment2.png" alt="Office" class="center accomplishment-img" />
+						</div>
+						<div class="col-md-6">
+							<h3>Office Building XYZ</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+							</p>	
+						</div>
+					</div>
+				</div>
+			</div>	
 		</div>	
+		<?php
+		include_once("footer.php");
+		?>
 	</body>
   </html>
