fbec9aaa1e1c61be0b28f0bbf3f1684efe1ee93f
Leyuan <leyuan.ly@outlook.com>
2015-04-16 00:00:52 -0600

    Updated floor-plan page

--- floor-plan.php
+++ floor-plan.php
@@ -39,19 +39,31 @@
 		<div class="sub-row fp-fp-container">
 			<div class="col-md-2 floor-selector-container">
 				<ul>
-					<li>6th Floor</li>
-					<li>5th Floor</li>
-					<li>4th Floor</li>
-					<li>3rd Floor</li>
-					<li>2nd Floor</li>
-					<li>Main</li>
+					<li>
+						<a class="floor-selector" data-floor="sixth-container"> 6th Floor </a>
+					</li>
+					<li>
+						<a class="floor-selector" data-floor="fifth-container"> 5th Floor </a>
+					</li>
+					<li>
+						<a class="floor-selector" data-floor="fourth-container"> 4th Floor </a>
+					</li>
+					<li>
+						<a class="floor-selector" data-floor="third-container"> 3rd Floor </a>
+					</li>
+					<li>
+						<a class="floor-selector" data-floor="second-container"> 2nd Floor </a>
+					</li>
+					<li>
+						<a class="floor-selector active" data-floor="main-container"> Main </a>
+					</li>
 				</ul>
 			</div>
-
-			<div class="container col-md-10 floor-img-container">
+			<!-- Main-floor -->
+			<div class="container col-md-10 floor-img-container main-container active">
 				<div class="floor-main">
 					<img src="assets/images/main-floor.png" alt="Main floor Image" class="floor-img" />
-					<div class="main-label main-row-one">
+					<div class="text-label label-row row-one main-row-one">
 						<span class="col-one">
 						</span>
 						<span class="col-two">
@@ -83,7 +95,7 @@
 							</p>
 						</span>
 					</div>
-					<div class="main-label main-row-two">
+					<div class="text-label label-row row-two main-row-two">
 						<span class="col-one">
 						</span>
 						<span class="col-two">
@@ -108,7 +120,7 @@
 							</p>
 						</span>
 					</div>
-					<div class="main-label main-row-three">
+					<div class="text-label label-row row-three main-row-three">
 						<span class="col-one">
 							<p>
 								<span class="status-available"></span>  102 <br />
@@ -143,10 +155,244 @@
 					</div>
 				</div>
 			</div>
+
+			<!-- Second Floor -->
+			<div class="container col-md-10 floor-img-container second-container">
+				<div class="floor-seond">
+					<img src="assets/images/second-floor.png" alt="Second floor Image" class="floor-img" />
+					<div class="text-label label-row row-one main-row-one">
+						<span class="col-one">
+						</span>
+						<span class="col-two">
+							<p>
+								<span class="status-available"></span>  104 <br />
+								SUITE B2 <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-three">
+							<p>
+								<span class="status-available"></span>  105 <br />
+								SUITE B <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-four">
+							<p>
+								<span class="status-available"></span>  106 <br />
+								SUITE B <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-five">
+							<p>
+								<span class="status-available"></span>  108 <br />
+								SUITE C4 <br />
+								1085 SQ. FT.
+							</p>
+						</span>
+					</div>
+					<div class="text-label label-row row-two main-row-two">
+						<span class="col-one">
+						</span>
+						<span class="col-two">
+							<p>
+								<span class="status-available"></span>  103 <br />
+								SUITE B2 <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-four">
+							<p>
+								<span class="status-available"></span>  107 <br />
+								SUITE B <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-five">
+							<p>
+								<span class="status-available"></span>  109 <br />
+								SUITE C4 <br />
+								1085 SQ. FT.
+							</p>
+						</span>
+					</div>
+					<div class="text-label label-row row-three main-row-three">
+						<span class="col-one">
+							<p>
+								<span class="status-available"></span>  102 <br />
+								SUITE B2 <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-two">
+							<p>
+								<span class="status-available"></span>  101 <br />
+								SUITE B2 <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-three">
+							
+						</span>
+						<span class="col-four">
+							<p>
+								<span class="status-available"></span>  111 <br />
+								SUITE B <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-five">
+							<p>
+								<span class="status-available"></span>  110 <br />
+								SUITE C4 <br />
+								1085 SQ. FT.
+							</p>
+						</span>
+					</div>
+				</div>
+			</div>
+
+			<!-- Third Floor -->
+			<div class="container col-md-10 floor-img-container third-container">
+				<div class="floor-seond">
+					<img src="assets/images/third-floor.png" alt="Second floor Image" class="floor-img" />
+					<div class="text-label label-row row-one main-row-one">
+						<span class="col-one">
+						</span>
+						<span class="col-two">
+							<p>
+								<span class="status-available"></span>  104 <br />
+								SUITE B2 <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-three">
+							<p>
+								<span class="status-available"></span>  105 <br />
+								SUITE B <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-four">
+							<p>
+								<span class="status-available"></span>  106 <br />
+								SUITE B <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-five">
+							<p>
+								<span class="status-available"></span>  108 <br />
+								SUITE C4 <br />
+								1085 SQ. FT.
+							</p>
+						</span>
+					</div>
+					<div class="text-label label-row row-two main-row-two">
+						<span class="col-one">
+						</span>
+						<span class="col-two">
+							<p>
+								<span class="status-available"></span>  103 <br />
+								SUITE B2 <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-four">
+							<p>
+								<span class="status-available"></span>  107 <br />
+								SUITE B <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-five">
+							<p>
+								<span class="status-available"></span>  109 <br />
+								SUITE C4 <br />
+								1085 SQ. FT.
+							</p>
+						</span>
+					</div>
+					<div class="text-label label-row row-three main-row-three">
+						<span class="col-one">
+							<p>
+								<span class="status-available"></span>  102 <br />
+								SUITE B2 <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-two">
+							<p>
+								<span class="status-available"></span>  101 <br />
+								SUITE B2 <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-three">
+							
+						</span>
+						<span class="col-four">
+							<p>
+								<span class="status-available"></span>  111 <br />
+								SUITE B <br />
+								970 SQ. FT.
+							</p>
+						</span>
+						<span class="col-five">
+							<p>
+								<span class="status-available"></span>  110 <br />
+								SUITE C4 <br />
+								1085 SQ. FT.
+							</p>
+						</span>
+					</div>
+				</div>
+			</div>			
 		</div>
 
 		<?php
 			include_once("footer.php");
 		?>
 	</body>
+
+	<script type="text/javascript">
+		var selector_class = document.getElementsByClassName("floor-selector");
+
+		for ( var i = 0; i < selector_class.length; i ++ ) {
+			selector_class[i].addEventListener('click', function(){
+				// console.log( this.className );
+				if( !this.className.match('active') ) {
+					// console.log( 'not active' );
+
+					// de-activate the current one
+					for ( var t = 0; t < selector_class.length; t++ ) {
+						if( selector_class[t].className.match('active') ) {
+							selector_class[t].classList.remove('active');
+						}
+					}
+
+					// hide the current floor div
+					var f = document.getElementsByClassName('floor-img-container');
+					// console.log( f );
+					for ( var d = 0; d < f.length; d++ ) {
+						if( f[d].className.match('active') ) {
+							f[d].classList.remove("active");
+						}
+					} 
+
+					// active the current one
+					this.classList.add('active');
+
+					// show the corresponding floor div
+					var corres_floor = this.getAttribute('data-floor');
+					for ( var d = 0; d < f.length; d++ ) {
+						if( f[d].className.match(corres_floor) ) {
+							f[d].classList.add("active");
+						}
+					} 
+				}
+			}, false);
+		}
+	</script>
   </html>
