568211d62bded32522ac48619ec9ca93ecef949b
Leyuan <leyuan.ly@outlook.com>
2016-04-10 23:53:15 -0600

    get in touch form

--- mail.php
+++ mail.php
@@ -1,17 +1,23 @@
 <?php
 require 'PHPMailer/PHPMailerAutoload.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name_from = strip_tags(trim($_POST["name"]));
-    $name_from = str_replace(array("\r","\n"),array(" "," "), $name_from);
-    $email_from = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $phone = trim($_POST["phone"]);
-    $message = trim($_POST["message"]);
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // fake
+$name_from = "User";
+$email_from = "user@mailinator.com";
+$phone = "123456";
+$message = "message";
+    // fake end
+    // $name_from = strip_tags(trim($_POST["name"]));
+    // $name_from = str_replace(array("\r","\n"),array(" "," "), $name_from);
+    // $email_from = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    // $phone = trim($_POST["phone"]);
+    // $message = trim($_POST["message"]);
 
-    if ( empty($name_from) OR empty($message) OR !filter_var($email_from, FILTER_VALIDATE_EMAIL)) {
-        echo "bad request";
-        exit;
-    }
+    // if ( empty($name_from) OR empty($message) OR !filter_var($email_from, FILTER_VALIDATE_EMAIL)) {
+    //     echo "bad request";
+    //     exit;
+    // }
 
     $mail = new PHPMailer(true);
 
@@ -40,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         var_dump($e);
         echo "fail";
     }
-}
+// }
 
 
 ?>
