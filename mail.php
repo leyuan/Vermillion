39e52d163022f3ebf9eb644e1027023dbed1a9cb
Leyuan <leyuan.ly@outlook.com>
2016-04-10 23:38:41 -0600

    get in touch form

--- mail.php
+++ mail.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Send();
         echo "success";
     } catch(Exception $e){
-        // var_dump($e);
+        var_dump($e);
         echo "fail";
     }
 }
