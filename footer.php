b19761e022337049221b3a78fcf02beea80e0608
Leyuan <leyuan.ly@gmail.com>
2016-11-13 16:12:24 -0700

    footer remake

--- footer.php
+++ footer.php
@@ -1,37 +1,59 @@
-<section id="footer">
-  <div class="container">
-    <div class="links">
+<div class="container sub-row" id="footer">
+  <div class="container links">
+    <div class="col-xs-6 col-md-6 col-md-3 text-center">
       <a href="index.php">
         <img src="assets/images/icons/home.jpg" alt="">
         <p>HOME</p>
       </a>
+    </div>
+    <div class="col-xs-6 col-md-6 col-md-3 text-center">
       <a href="neighborhood.php">
         <img src="assets/images/icons/neighborhood.jpg" alt="">
         <p>NEIGHBORHOOD</p>
       </a>
-      <div class="small-show"></div>
+    </div>
+    <div class="col-xs-6 col-md-6 col-md-3 text-center">
       <a href="floor-plan.php">
         <img src="assets/images/icons/floorplan.jpg" alt="">
         <p>FLOOR PLANS</p>
       </a>
-      <!-- <a href="about-us.php">
-        <img src="assets/images/icons/aboutus.jpg" alt="">
-        <p>ABOUT US</p>
-      </a> -->
+    </div>
+    <div class="col-xs-6 col-md-6 col-md-3 text-center">
       <a href="get-in-touch.php">
         <img src="assets/images/icons/getintouch.jpg" alt="">
         <p>GET IN TOUCH</p>
       </a>
     </div>
+  </div>
 
-    <div class="info">
-      <p>+1 (780)-475-5999</p>
+  <div class="container">
+    <div class="col-xs-6 col-sm-6 col-md-4 text-center">
+      <h4><u>Vermillion Condo</u></h4>
+
       <p>10611 116 STREET NW, EDMONTON</p>
-      <p>COPYRIGHT © RED LEAF LTD. 2016. ALL RIGHTS RESERVED.</p>
+      <p>(780)-475-5999</p>
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-4 text-center">
+      <h4><u>Sales Center</u></h4>
+      <p>
+        <p>10351 92 Street NW, Edmonton</p>
+        <p>(780)-475-5999 </p>
+        <p>info@redleafcanada.com</p>
+        <p>Mon-Thurs 3pm-7pm</p>
+        <p>Weekends 1pm-5pm</p>
+        <p>Close on Friday & Holidays</p>
+      </p>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-4 text-center">
+      <p>
+        <img src="assets/images/red-leaf-logo.png" class="red-leaf-logo"/>
+      </p>
+      <p>COPYRIGHT © RED LEAF LTD. 2016</p>
+      <p>ALL RIGHTS RESERVED.</p>
     </div>
   </div>
-</section>
-<!-- <script src="assets/js/jquery.min.js"></script> -->
+</div>
+
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/pgwslideshow.js"></script>
 
